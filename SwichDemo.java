2135a089ab135ef8180ec81131a7307a6fb99231
baru980 <155515480+baru980@users.noreply.github.com>
2024-01-04 20:40:15 +0900

    condition (조건문)
    조건문에 과한 예제

---
+++ SwichDemo.java
@@ -0,0 +1,30 @@
+package org.opentutorials.javatutorials.condition;
+
+public class SwichDemo {
+	
+	public static void main(String[] args) {
+		System.out.println("switch(1)");
+		switch(3){
+		case 1:
+			System.out.println(1);
+			break;
+		case 2:
+			System.out.println(2);
+			break;
+		}
+			
+			
+		System.out.println("switch(2)");
+		switch(2){
+		case 1:
+			System.out.println(1);
+			break;
+		case 2:
+			System.out.println(2);
+			break;
+			}
+		}
+
+	}
+
+
---
+++ loginDemo.java
@@ -0,0 +1,18 @@
+package org.opentutorials.javatutorials.condition;
+
+public class loginDemo {
+
+	public static void main(String[] args) {
+		String id = "egoing";
+		
+		if (id.equals("egoing")){
+				System.out.println("right");
+		}else {
+			 	System.out.println("false");
+					
+
+		}
+
+	}
+
+}
