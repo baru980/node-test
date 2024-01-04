a342049d1e60f4056cc89f86d948b53b99b89a61
baru980 <155515480+baru980@users.noreply.github.com>
2024-01-04 20:42:42 +0900

    variables 변수
    변수에 관한 예쪠

---
+++ ConstantDemo.java
@@ -0,0 +1,14 @@
+package org.opentutorials.javatutorials.variables;
+
+public class ConstantDemo {
+
+	public static void main(String[] args) {
+
+		float a = 2.2f;
+		long b = 2147483648L;
+		
+		double c = 2.2f;
+	}
+
+}
+ 
