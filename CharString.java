4c0fecc24bfa66ae97da17a4101acf6b86152c72
baru980 <155515480+baru980@users.noreply.github.com>
2024-01-04 20:41:48 +0900

    Numbering 문자열
    문자열에 관한 예제

---
+++ CharString.java
@@ -0,0 +1,10 @@
+package org.opentutorials.javatutorials.numberString;
+
+public class CharString {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello\"생활코딩입니다\" excalf fkdof \n asdfg \n dfpglsd");
+	}
+
+}
---
+++ Number.java
@@ -0,0 +1,16 @@
+package org.opentutorials.javatutorials.numberString;
+
+public class Number {
+
+	public static void main(String[] args) {
+		
+		int i = 2;
+		int j = 2;
+		
+		System.out.println(i+j);
+		System.out.println(i-j);
+		System.out.println(i*j);
+		System.out.println(i/j);
+	}
+
+}
